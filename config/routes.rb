f7821f6766bf094b99829a0c62d63688a568c755
ukyo ohashi <u.ohashi@opt.ne.jp>
2023-06-27 13:55:00 +0900

    add: about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
+  get  'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "application#hello"
+  root 'application#hello'
 end
