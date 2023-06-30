19047e9e3d012ae02d11a68d75bc6cd817d8ba65
ukyo ohashi <u.ohashi@opt.ne.jp>
2023-06-30 10:36:52 +0900

    chapter7 done

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users
+
   # Defines the root path route ("/")
   # root "articles#index"
   root 'static_pages#home'
